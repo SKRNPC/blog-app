efca9c5eeec481fd9dbde0448529971cc906c407
skrnpc <kirnapcsinan@gmail.com>
2024-05-06 16:08:14 +0300

    navigate işlemi yapıldı

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import SignUp from "./components/SignUp";
 
@@ -11,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<Navigate replace to="/login" />} />
         </Routes>
       </Router>
     </div>
