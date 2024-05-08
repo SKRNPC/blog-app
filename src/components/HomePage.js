a9b5b7ba3a25149c051945ff36563bcff51e3053
skrnpc <kirnapcsinan@gmail.com>
2024-05-08 13:53:40 +0300

    mobil cihazlar için tasarım değiştirildi

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,26 +18,26 @@ function HomePage() {
   console.log(users.blogs);
   return (
     <>
-      <div className="d-flex flex-column align-items-center mt-2 ">
+      <div className="d-flex flex-column align-items-center ">
         {users.map((item) => (
           <div
-            className="col-11 col-md-10 col-lg-9 col-xl-8 p-3 p-lg-5 "
+            className="col-11 col-md-10 col-lg-9 col-xl-8 p-3 p-lg-5 mb-4"
             style={{ backgroundColor: "#fff8f5" }}
           >
-            <div className="bg-white d-flex flex-row justify-content-between">
-              <div className=" d-flex flex-column justify-content-between  w-100 ">
+            <div className="bg-white d-flex flex-sm-row flex-column justify-content-between">
+              <div className=" d-flex flex-column justify-content-between  w-100 order-sm-1 order-2 ">
                 <div className="">
-                  <div className="col d-flex align-items-center p-sm-2 p-md-3 p-1 fw-bold ">
-                    <h3 className="m-0">{item.blog_name}</h3>
+                  <div className="col d-flex align-items-center p-sm-2 p-md-3 p-1 fw-bolder ">
+                    <h1 className="m-0 display-3 ">{item.blog_name}</h1>
                   </div>
                   <div className="col d-flex align-items-center  p-md-4 p-sm-3 p-2 ">
-                    <p className=" fs-6 lead m-0">{item.article}</p>
+                    <p className="font m-0">{item.article}</p>
                   </div>
                 </div>
                 <div className="p-md-3 p-sm-2 p-1 ">
                   <button
-                    className="btn d-flex w-lg-25  text-white fw-bold rounded-0 "
-                    style={{ backgroundColor: "#9B6262", width: "110px" }}
+                    className="btn d-flex text-white fw-bold rounded-0 justify-content-center "
+                    style={{ backgroundColor: "#9B6262", width: "150px" }}
                   >
                     Read More
                   </button>
@@ -46,8 +46,7 @@ function HomePage() {
               <img
                 src={item.image || defaultImage}
                 alt="Görüntü"
-                style={{ width: "40%", height: "40%" }}
-                className="ms-1"
+                className="ms-sm-1 element order-sm-2 order-1"
               />
             </div>
           </div>
