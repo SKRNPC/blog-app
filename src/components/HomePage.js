61ba781cf68caebd5bb4e5c4e4238df831b53849
skrnpc <kirnapcsinan@gmail.com>
2024-05-08 11:56:47 +0300

    Jsondan veri çekme işlemi yapıldı

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,52 @@
+import { useEffect, useState } from "react";
 import defaultImage from "../images/default-img.jpg";
+import axios from "axios";
 
 function HomePage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/home_active.json")
+      .then((response) => {
+        setUsers(response.data.blogs);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the Axios request:", error);
+      });
+  }, []);
+  console.log(users.blogs);
   return (
     <>
       <div className="d-flex flex-column align-items-center ">
-        <div className="bg-secondary d-flex flex-row justify-content-between col-7 p-3 ">
-          <div className="p-3 d-flex flex-column ">
-            <div className="p-3 fw-bold ">
-              <h1>BlogName</h1>
-            </div>
-            <div>
-              <p className="lead">Article</p>
+        {users.map((item) => (
+          <div className="bg-light  col-7 p-5 ">
+            <div className="bg-white d-flex flex-row justify-content-between">
+              <div className="p-3 d-flex flex-column justify-content-between w-100 ">
+                <div className="">
+                  <div className="col p-3 fw-bold ">
+                    <h1>{item.blog_name}</h1>
+                  </div>
+                  <div className="col p-3">
+                    <p className="lead">{item.article}</p>
+                  </div>
+                </div>
+                <button
+                  className="btn p-3 w-25 text-white fw-bold rounded-0 "
+                  style={{ backgroundColor: "#9B6262" }}
+                >
+                  Read More
+                </button>
+              </div>
+              <img
+                src={item.image || defaultImage}
+                alt="Görüntü"
+                style={{ width: "40%" }}
+                className="ms-2"
+              />
             </div>
           </div>
-          <img src={defaultImage} alt="" style={{ width: "40%" }}></img>
-        </div>
+        ))}
       </div>
     </>
   );
