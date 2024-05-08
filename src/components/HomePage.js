315c4b215bc70d894dfa0da1d9e2f6b44bc6f1e2
skrnpc <kirnapcsinan@gmail.com>
2024-05-08 13:08:28 +0300

    responsive tasarım yapıldı

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,31 +18,36 @@ function HomePage() {
   console.log(users.blogs);
   return (
     <>
-      <div className="d-flex flex-column align-items-center ">
+      <div className="d-flex flex-column align-items-center mt-2 ">
         {users.map((item) => (
-          <div className="bg-light  col-7 p-5 ">
+          <div
+            className="col-11 col-md-10 col-lg-9 col-xl-8 p-3 p-lg-5 "
+            style={{ backgroundColor: "#fff8f5" }}
+          >
             <div className="bg-white d-flex flex-row justify-content-between">
-              <div className="p-3 d-flex flex-column justify-content-between w-100 ">
+              <div className=" d-flex flex-column justify-content-between  w-100 ">
                 <div className="">
-                  <div className="col p-3 fw-bold ">
-                    <h1>{item.blog_name}</h1>
+                  <div className="col d-flex align-items-center p-sm-2 p-md-3 p-1 fw-bold ">
+                    <h3 className="m-0">{item.blog_name}</h3>
                   </div>
-                  <div className="col p-3">
-                    <p className="lead">{item.article}</p>
+                  <div className="col d-flex align-items-center  p-md-4 p-sm-3 p-2 ">
+                    <p className=" fs-6 lead m-0">{item.article}</p>
                   </div>
                 </div>
-                <button
-                  className="btn p-3 w-25 text-white fw-bold rounded-0 "
-                  style={{ backgroundColor: "#9B6262" }}
-                >
-                  Read More
-                </button>
+                <div className="p-md-3 p-sm-2 p-1 ">
+                  <button
+                    className="btn d-flex w-lg-25  text-white fw-bold rounded-0 "
+                    style={{ backgroundColor: "#9B6262", width: "110px" }}
+                  >
+                    Read More
+                  </button>
+                </div>
               </div>
               <img
                 src={item.image || defaultImage}
                 alt="Görüntü"
-                style={{ width: "40%" }}
-                className="ms-2"
+                style={{ width: "40%", height: "40%" }}
+                className="ms-1"
               />
             </div>
           </div>
